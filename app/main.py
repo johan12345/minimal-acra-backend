982f3bf01801f3d62ab7521e5f165405a301b085
johan12345 <johan.forstner@gmail.com>
2023-09-02 16:26:49 +0200

    add email send functionality

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import json
 import os
+from smtplib import SMTP
+from textwrap import dedent, indent
 
 from flask import Flask, request
 
@@ -6,6 +9,26 @@ app = Flask(__name__)
 
 user = os.environ['ACRA_BASIC_USER']
 password = os.environ['ACRA_BASIC_PASSWORD']
+smtp_host = os.environ['SMTP_HOST']
+smtp_port = int(os.environ['SMTP_PORT'])
+smtp_user = os.environ['SMTP_USER']
+smtp_password = os.environ['SMTP_PASSWORD']
+smtp_from = os.environ['SMTP_FROM']
+smtp_to = os.environ['SMTP_TO']
+
+
+def format(acra_data):
+    text = ""
+    for key in acra_data:
+        text += f"{key}="
+        value = acra_data[key]
+        if isinstance(value, str) or isinstance(value, int) or isinstance(value, float) or isinstance(value, bool):
+            text += str(value) + "\n"
+        elif isinstance(value, list):
+            text += json.dumps(value) + "\n"
+        else:
+            text += "\n" + indent(format(value), "  ")
+    return text
 
 
 @app.post('/report')
@@ -15,10 +38,22 @@ def submit_report():
     if request.authorization.username != user or request.authorization.password != password:
         return 'unauthorized', 401
 
-    request_data = request.get_json()
-    print(request_data)
+    acra_data = request.get_json()
 
-    # TODO: send email
+    package_name = acra_data["PACKAGE_NAME"]
+    app_version_name = acra_data["APP_VERSION_NAME"]
+
+    with SMTP(smtp_host, smtp_port) as smtp:
+        smtp.starttls()
+        smtp.login(smtp_user, smtp_password)
+        smtp.sendmail(
+            smtp_from, smtp_to,
+            dedent(f"""\
+            From: {smtp_from}
+            Subject: {package_name} {app_version_name} Crash Report
+            
+            """) + format(acra_data)
+        )
 
     return ""
 
